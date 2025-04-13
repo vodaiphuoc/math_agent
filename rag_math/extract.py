57733e1c3b5f14ee89de7ab9f7afaf55522cdfaa
vodaiphuoc <phuocvodn98gmail.com>
2025-04-13 22:57:43 +0100

    adjsut batch

--- rag_math/extract.py
+++ rag_math/extract.py
@@ -47,8 +47,8 @@ into any other languages.
 
     def __init__(self):
         self._engine = JAXExtractModel()
-        self.num_batch = 25
-    
+        self.num_batch = 30
+
     def run(self, pdf_path: str)->str:
         
         img_paths = _pdf2imgs(pdf_path)
